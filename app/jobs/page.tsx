9a6c5e47c515ab33fc76c3169d924770737c5c5b
Marlon  Mello <marlonmello@Marlons-iMac.local>
2026-04-08 22:36:01 -0400

    add jobs page

---
+++ app/jobs/page.tsx
@@ -0,0 +1,3 @@
+export default function JobsPage() {
+  return <div style={{ padding: 40 }}>Jobs page working</div>
+}
---
+++ app/test123/page.tsx
@@ -0,0 +1,3 @@
+export default function Test123Page() {
+  return <div style={{ padding: 40 }}>Test123 works</div>
+}
